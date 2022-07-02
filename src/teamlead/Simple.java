99d494d97ddedd985babace0394a4b37a6713dff
9731Kumar <kalyankumara2502@gmail.com>
2022-07-02 21:46:27 +0530

    First commit

---
+++ src/teamlead/Simple.java
@@ -0,0 +1,12 @@
+package teamlead;
+
+public class Simple {
+	
+	public static void main(String[] args) {
+		Simple simple= new Simple();
+		System.out.println("This is simple project;");
+		System.out.println("Its is created by Teamlead, he will upload this project to "
+				+ "github, after that team members will pull it and modify the code");
+	}
+
+}
